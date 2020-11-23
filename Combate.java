77324e05f05f1f853c88af590370693b408d56d4
Claudio Henríquez <63313873+ClaudioHenriquezBerroeta@users.noreply.github.com>
2020-11-23 14:59:41 -0300

    update

--- Combate.java
+++ Combate.java
@@ -12,7 +12,6 @@ public class Combate{
 		buffer = new BufferedReader(new InputStreamReader(System.in));
 		this.player1 = player1;
 		indexRival = (int)Math.round(Math.random()*10);
-		System.out.println(indexRival);
 		this.player2 = pokedexGral.getPokemon(indexRival);
 	}
 
@@ -20,12 +19,15 @@ public class Combate{
 		int op;
 		try{
 			System.out.println(player1.getNombre()+", El ataque será: ");
-			System.out.println("1.-"+player1.getAtaques()[0]);
-			System.out.println("2.-"+player1.getAtaques()[1]);
-			System.out.println("3.-"+player1.getAtaques()[2]);
+			System.out.println("1.-"+player1.getAtaques()[0].getNombre());
+			System.out.println("2.-"+player1.getAtaques()[1].getNombre());
+			System.out.println("3.-"+player1.getAtaques()[2].getNombre());
 			op = Integer.parseInt(buffer.readLine());
 
 			player2.recibirAtaque(player1.getAtaques()[op-1]);
+			System.out.println("\n\n********************************************************\n");
+			System.out.println(player2.getNombre()+" tu nivel de Puntos de Salud es "+player2.getPs());
+			System.out.println("\n********************************************************\n\n");
 		}catch(IOException e){
 			System.out.println("Error de lectura desde el teclado...");
 		}
@@ -37,7 +39,9 @@ public class Combate{
 		while(sigue){
 			ataque(player1,player2);
 			if(revisarTriunfo(player2)){
+				System.out.println("\n\n********************************************************\n\n");
 				System.out.println("Felicitaciones...Haz ganado la partida!!");
+				System.out.println("\n\n********************************************************\n\n");
 				out = indexRival;
 				player1.restaurar();
 				player2.restaurar();
@@ -45,7 +49,9 @@ public class Combate{
 			} 
 			ataque(player2,player1);
 			if(revisarTriunfo(player1)){
+				System.out.println("\n\n********************************************************\n\n");
 				System.out.println("Haz perdido la partida!!");
+				System.out.println("\n\n********************************************************\n\n");
 				out = -1;
 				player1.restaurar();
 				player2.restaurar();
